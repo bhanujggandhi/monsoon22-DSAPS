289d6f0bc87774d87ae524f513b3416167f94dd4
bhanujggandhi <gandhibhanuj@gmail.com>
2022-08-15 23:22:00 +0530

    big addition implemented

--- q1/q1.cpp
+++ q1/q1.cpp
@@ -3,12 +3,90 @@
 
 using namespace std;
 
-string bigintadd(string &num1, string &num2)
+void reverse_string(string &s)
 {
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++)
+    {
+        char temp = s[i];
+        s[i] = s[n - i - 1];
+        s[n - i - 1] = temp;
+    }
+    return;
+}
+
+string bigintadd(string num1, string num2)
+{
+    reverse_string(num1);
+    reverse_string(num2);
+    string ans;
+
+    int size1 = num1.size();
+    int size2 = num2.size();
+    int i = 0, j = 0, carry = 0;
+
+    cout << size1 << endl;
+    cout << size2 << endl;
+
+    while (i < size1 and j < size2)
+    {
+        int a = num1[i++] - '0';
+        int b = num2[j++] - '0';
+        int c = (a + b + carry) % 10;
+        carry = (a + b + carry) / 10;
+        ans.push_back(c + '0');
+    }
+
+    cout << i << "    " << j << endl;
+
+    while (i < size1)
+    {
+        int a = num1[i++] - '0';
+        int c = (a + carry) % 10;
+        carry = (a + carry) / 10;
+        ans.push_back(c + '0');
+    }
+
+    while (j < size2)
+    {
+        int a = num2[j++] - '0';
+        int c = (a + carry) % 10;
+        carry = (a + carry) / 10;
+        ans.push_back(c + '0');
+    }
+
+    reverse_string(ans);
+    return ans;
 }
 
 int main()
 {
+    int n;
+    cin >> n;
+    string inp;
+    cin >> inp;
+
+    // switch (n)
+    // {
+    // case 1:
+    //     cout << "Called 1";
+    //     break;
+    // case 2:
+    //     cout << "Called 2";
+    //     break;
+    // case 3:
+    //     cout << "Called 3";
+    //     break;
+    // case 4:
+    //     cout << "Called 4";
+    //     break;
+    // default:
+    //     cout << "Called default";
+    //     break;
+    // }
+
+    string ans = bigintadd("12345678910111213141516", "302928272625242322212019181716");
+    cout << ans << endl;
 
     return 0;
 }
