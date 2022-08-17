5977594408a858cf7c5a754f60cc4d0fe1c9fd73
bhanujggandhi <gandhibhanuj@gmail.com>
2022-08-17 16:41:10 +0530

    gcd step 1: Energy function implemented

--- q3/src/main.cpp
+++ q3/src/main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
-void solve(int ***rgb, int H, int W, int C, int H_, int W_, int C_)
+// 1. Calculate Energy
+void compute_energy(int ***rgb, double **energy, int H, int W)
 {
     for (int i = 0; i < H; i++)
     {
         for (int j = 0; j < W; j++)
         {
-            for (int k = 0; k < C; k++)
-            {
-                cout << rgb[i][j][k] << "  ";
-            }
-            cout << endl;
+            // del x
+            int rx = rgb[i][(j - 1 + W) % W][0] - rgb[i][(j + 1) % W][0];
+            int bx = rgb[i][(j - 1 + W) % W][1] - rgb[i][(j + 1) % W][1];
+            int gx = rgb[i][(j - 1 + W) % W][2] - rgb[i][(j + 1) % W][2];
+
+            int delx = rx * rx + bx * bx + gx * gx;
+
+            // del y
+            int ry = rgb[(i - 1 + H) % H][j][0] - rgb[(i + 1) % H][j][0];
+            int by = rgb[(i - 1 + H) % H][j][1] - rgb[(i + 1) % H][j][1];
+            int gy = rgb[(i - 1 + H) % H][j][2] - rgb[(i + 1) % H][j][2];
+
+            int dely = ry * ry + by * by + gy * gy;
+
+            energy[i][j] = sqrt(delx + dely);
         }
+    }
+}
+
+// 2. Seam Identification
+
+// 3. Seam Removal
+
+void solve(int ***rgb, int H, int W, int C, int H_, int W_, int C_)
+{
+    double **energy = new double *[H];
+
+    for (int i = 0; i < H; i++)
+        energy[i] = new double[W];
+
+    compute_energy(rgb, energy, H, W);
+
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < W; j++)
+            cout << energy[i][j] << "   ";
         cout << endl;
     }
 }
