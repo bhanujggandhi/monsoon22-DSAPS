dd826d9b6c6e00a3d4e26fa2c7d7c4fa29640cd2
bhanujggandhi <gandhibhanuj@gmail.com>
2022-09-26 15:31:16 +0530

    Q1: autocomplete

--- assignment_3/q1/q1.cpp
+++ assignment_3/q1/q1.cpp
@@ -38,6 +38,43 @@ bool spell_check(Node* root, string& word) {
     return false;
 }
 
+void getWords(Node* root, vector<string>& suggestions, string currword) {
+    if (root->isWord) suggestions.push_back(currword);
+
+    for (int i = 0; i < 26; i++) {
+        if (root->dictionary[i] == NULL) continue;
+
+        currword.push_back(i + 'a');
+        getWords(root->dictionary[i], suggestions, currword);
+        currword.pop_back();
+    }
+}
+
+vector<string> autocomplete(Node* root, string& word) {
+    vector<string> suggestions;
+    Node* curr = root;
+    string currword;
+    for (int i = 0; i < word.size(); i++) {
+        if (curr->dictionary[word[i] - 'a'] == NULL)
+            return suggestions;
+        else {
+            currword.push_back(word[i]);
+            curr = curr->dictionary[word[i] - 'a'];
+        }
+    }
+
+    Node* temp = curr;
+    for (int i = 0; i < 26; i++) {
+        if (temp->dictionary[i] == NULL) continue;
+
+        currword.push_back(i + 'a');
+        getWords(temp->dictionary[i], suggestions, currword);
+        currword.pop_back();
+    }
+
+    return suggestions;
+}
+
 int main() {
     int n, q;
     cin >> n >> q;
@@ -49,8 +86,12 @@ int main() {
     Node* root = new Node();
     buildTrie(root, dict);
 
-    string str = "dame";
-    cout << spell_check(root, str) << endl;
+    string str = "fi";
+
+    cout << "---------------" << endl;
+    vector<string> ans = autocomplete(root, str);
+    cout << ans.size() << endl;
+    for (auto x : ans) cout << x << " ";
 
     return 0;
 }
