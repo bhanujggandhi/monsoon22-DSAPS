712c9978ed4ef7fb65bd7516416dfe6821cc7d04
bhanujggandhi <gandhibhanuj@gmail.com>
2022-09-27 17:11:21 +0530

    Q1 levenshtein distance autocorrect implemented

--- assignment_3/q1/q1.cpp
+++ assignment_3/q1/q1.cpp
@@ -71,6 +71,67 @@ vector<string> autocomplete(Node* root, string& word) {
     return suggestions;
 }
 
+void autocorrectHelper(Node* root, char c, string& word, vector<int>& prevRow,
+                       vector<string>& ans, string& currword) {
+    vector<int> t;
+    t.push_back(prevRow[0] + 1);
+
+    for (int i = 1; i < word.size() + 1; i++) {
+        int insertop = t[i - 1] + 1;
+        int deleteop = prevRow[i] + 1;
+        int replaceop = 0;
+        if (word[i - 1] != c) {
+            replaceop = prevRow[i - 1] + 1;
+        } else
+            replaceop = prevRow[i - 1];
+
+        t.push_back(min(insertop, min(deleteop, replaceop)));
+    }
+
+    if (t[word.size()] <= 3 and root->isWord) {
+        ans.push_back(currword);
+    }
+
+    int currminentry = INT32_MAX;
+    for (auto x : t) currminentry = min(x, currminentry);
+
+    if (currminentry <= 3) {
+        for (int i = 0; i < 26; i++) {
+            if (root->dictionary[i] != NULL) {
+                char temp = i + 'a';
+                currword.push_back(temp);
+                autocorrectHelper(root->dictionary[i], temp, word, t, ans,
+                                  currword);
+                currword.pop_back();
+            }
+        }
+    }
+}
+
+vector<string> autocorrect(Node* root, string& word) {
+    vector<int> t(word.size() + 1);
+
+    // Top row
+    for (int i = 0; i < t.size(); i++) {
+        t[i] = i;
+    }
+
+    vector<string> ans;
+
+    string currword;
+    for (int i = 0; i < 26; i++) {
+        if (root->dictionary[i] != NULL) {
+            char temp = i + 'a';
+            currword.push_back(temp);
+            autocorrectHelper(root->dictionary[i], temp, word, t, ans,
+                              currword);
+            currword.pop_back();
+        }
+    }
+
+    return ans;
+}
+
 int main() {
     int n, q;
     cin >> n >> q;
@@ -82,12 +143,30 @@ int main() {
         buildTrie(root, word);
     }
 
-    string str = "fi";
+    while (q--) {
+        int a;
+        cin >> a;
+        string word;
+        cin >> word;
 
-    cout << "---------------" << endl;
-    vector<string> ans = autocomplete(root, str);
-    cout << ans.size() << endl;
-    for (auto x : ans) cout << x << " ";
+        if (a == 1) {
+            cout << spell_check(root, word) << endl;
+        } else if (a == 2) {
+            vector<string> suggestions;
+            suggestions = autocomplete(root, word);
+            cout << suggestions.size() << endl;
+            for (int i = 0; i < suggestions.size(); i++) {
+                cout << suggestions[i] << endl;
+            }
+        } else if (a == 3) {
+            vector<string> suggestions;
+            suggestions = autocorrect(root, word);
+            cout << suggestions.size() << endl;
+            for (int i = 0; i < suggestions.size(); i++) {
+                cout << suggestions[i] << endl;
+            }
+        }
+    }
 
     return 0;
 }
@@ -104,5 +183,8 @@ dames
 filling
 grasses
 fitter
-
+1 litter
+1 dame
+2 con
+3 filter
  */
