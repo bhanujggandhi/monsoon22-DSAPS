68b08e50d842dbcbb8112db434aaeaf2e250ff4d
bhanujggandhi <gandhibhanuj@gmail.com>
2022-09-30 15:51:39 +0530

    Q2: heap implemented

--- assignment_3/q2/test.cpp
+++ assignment_3/q2/test.cpp
@@ -1,29 +1,7 @@
-#include <stdio.h>
-#include <string.h>
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-
-#define CHUNK_SIZE 1024000
+#include <bits/stdc++.h>
 
 using namespace std;
 
-/*
-    -> the primary goal of an external sorting algorithm is to minimize the
-   number of times information must be read from or written to disk
-
-   ->Under good conditions, reading from a file in sequential order is more
-   efficient than reading blocks in random order.
-
-*/
-
-typedef long long ll;
-
-/*
-0-based heap
-if i->parent then 2i+1, 2i+2 are children
-if i is child then (i-1)/2 is parent
-*/
 struct Pair {
     long long value;
     ifstream ind;
@@ -74,7 +52,7 @@ class MaxHeap {
             return {arr[size].value, move(arr[size].ind)};
         }
 
-        ll maxval = arr[0].value;
+        long long maxval = arr[0].value;
         ifstream maxfs = move(arr[0].ind);
         // replace root by last element
         swap(&arr[0], &arr[size - 1]);
@@ -120,71 +98,28 @@ class MaxHeap {
     }
 };
 
-long long divide(string inputfilename) {
-    /*
-        1. open input file
-        2. Read first k bytes
-        3. While not end of the file:
-            open new output file
-            write k bytes
-            close output file
-            read next k block
-    */
+int main() {
+    MaxHeap hp(10);
+    ifstream fs("input.txt");
+    hp.insert(32, move(fs));
+    hp.insert(92, move(fs));
+    hp.insert(1000, move(fs));
+    hp.insert(313221, move(fs));
+    hp.insert(3083198, move(fs));
+    hp.insert(3081, move(fs));
+    hp.insert(2380, move(fs));
 
-    long long ind = 0;
-
-    fstream inputfile(inputfilename, std::ios_base::in);
-
-    for (ind = 0; inputfile.peek() != EOF; ind++) {
-
-        vector<long long> buff(CHUNK_SIZE);
-
-        long long a;
-
-        for (long long i = 0; i < CHUNK_SIZE; i++) {
-            if (inputfile >> a) {
-                buff[i] = a;
-            } else {
-                break;
-            }
-        }
-
-        // Conquer
-        sort(buff.begin(), buff.end());
-        string chunkfilename = "temp" + to_string(ind) + ".txt";
-        ofstream out_file{chunkfilename};
-
-        for (auto x : buff) {
-            out_file << x << " ";
-        }
-
-        out_file.close();
-    }
-    inputfile.close();
-
-    return ind;
-}
-
-void mergefiles(long long totalfiles) {
-    vector<ifstream> filesarr(totalfiles);
-
-    for (long long i = 0; i < totalfiles; i++) {
-        string chunkfilename = "temp" + to_string(i) + ".txt";
-        ifstream file(chunkfilename, std::ios_base::in);
-        filesarr[i] = move(file);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    // if (argc != 3) {
-    //     cout << "Please enter two arguments only" << endl;
-    //     return 1;
-    // }
-    string inp = "input.txt";
-    long long totalfiles = divide(inp);
-
-    cout << totalfiles << endl;
-    mergefiles(totalfiles);
+    cout << hp.remove().value << endl;
+    cout << hp.remove().value << endl;
+    cout << hp.remove().value << endl;
+    cout << hp.remove().value << endl;
+    cout << hp.remove().value << endl;
+    ifstream f = hp.remove().ind;
+    long long a;
+    f >> a;
+    cout << a;
+    f >> a;
+    cout << a;
 
     return 0;
 }
